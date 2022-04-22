0165921c698d8b75b45f64e8577ec08ec04afbc0
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2022-04-22 23:29:20 +0200

    Remove empty source files

--- src/gfx2d/style.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2021-2022 Omar Boukli-Hacene. All rights reserved.
-// Distributed under an MIT-style license that can be
-// found in the LICENSE file.
-
-// SPDX-License-Identifier: MIT
-
-#include "style.h"
