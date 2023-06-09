b2c1109ef9ee08f74a2315491fb664b6c21aabe1
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-06-09 18:23:30 +0200

    refactor: Fix naming inconsistencies

--- src/cli/init.hpp
+++ src/cli/init.hpp
@@ -15,7 +15,7 @@ void init(int const size, int const x, int const y) noexcept;
 
 #ifdef _WINDOWS
 void init(
-    int const size, int const x, int const y,
+    int const order, int const x, int const y,
     emulation_mode_type const emulation_mode) noexcept;
 #endif // _WINDOWS
 
