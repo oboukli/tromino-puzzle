751329e9fc5236cf06cd46b277cfc8d666f3f688
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-07-05 19:10:06 +0200

    refactor: Limit to one exit point per function

--- src/gui/cli_options.cpp
+++ src/gui/cli_options.cpp
@@ -29,26 +29,29 @@ bool read_options(
     using namespace std::string_literals;
     using namespace tromino::tromino2d::params;
 
+    bool has_error{true};
     if (argc < REQUIRED_ARG_COUNT) {
         error = "Incorrect argument count."s;
 
-        return true;
+        has_error = true;
+    } else {
+        options.order = std::stoi(argv[ORDER_ARG_IDX]);
+        options.x = std::stoi(argv[MARKX_ARG_IDX]);
+        options.y = std::stoi(argv[MARKY_ARG_IDX]);
+        options.force = (argc > REQUIRED_ARG_COUNT)
+            && (std::string(argv[FORCE_ARG_IDX]) == "-f"s);
+
+        if ((options.order > MAX_SUPPORTED_ORDER) && (!options.force)) {
+            error
+                = "Order is too large that it might crash the app. Use the -f option to force the puzzle."s;
+
+            has_error = true;
+        } else {
+            has_error = false;
+        }
     }
 
-    options.order = std::stoi(argv[ORDER_ARG_IDX]);
-    options.x = std::stoi(argv[MARKX_ARG_IDX]);
-    options.y = std::stoi(argv[MARKY_ARG_IDX]);
-    options.force = (argc > REQUIRED_ARG_COUNT)
-        && (std::string(argv[FORCE_ARG_IDX]) == "-f"s);
-
-    if ((options.order > MAX_SUPPORTED_ORDER) && (!options.force)) {
-        error
-            = "Order is too large that it might crash the app. Use the -f option to force the puzzle."s;
-
-        return true;
-    }
-
-    return false;
+    return has_error;
 }
 
 } // namespace tromino::tromino2d
