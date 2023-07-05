b8d8c5e8db27b9b62649ba84859ea2bec832bc74
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-07-05 20:43:34 +0200

    refactor(tromino): Limit to one exit point

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -14,7 +14,8 @@
 #include "init.hpp"
 
 int main(int const argc, char const* const argv[]) noexcept {
-    tromino::cli::options options;
+    int exit_status{EXIT_FAILURE};
+    tromino::cli::options options{};
 
     std::string error;
     if (bool const is_error{
@@ -23,16 +24,16 @@ int main(int const argc, char const* const argv[]) noexcept {
         || (!::trmn_is_valid_config(options.order, options.x, options.y))) {
         std::cerr << error << std::endl;
         tromino::cli::print_usage(std::cout);
-
-        return EXIT_FAILURE;
-    }
-
+    } else {
 #ifdef _WINDOWS
-    tromino::cli::app::init(
-        options.order, options.x, options.y, options.emulation_mode);
+        tromino::cli::app::init(
+            options.order, options.x, options.y, options.emulation_mode);
 #else
-    tromino::cli::app::init(options.order, options.x, options.y);
+        tromino::cli::app::init(options.order, options.x, options.y);
 #endif // _WINDOWS
 
-    return EXIT_SUCCESS;
+        exit_status = EXIT_SUCCESS;
+    }
+
+    return exit_status;
 }
