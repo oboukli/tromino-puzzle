81ae4ba92340eb9052d9f180d533d581cabb50b6
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-10-23 22:10:17 +0200

    refactor: Revisit const correctness

--- src/cli/cli_options.hpp
+++ src/cli/cli_options.hpp
@@ -17,8 +17,8 @@ namespace tromino::cli {
 void print_usage(std::ostream& os) noexcept;
 
 bool read_options(
-    int const argc,
-    char const* const argv[],
+    int argc,
+    char const* argv[],
     options& options,
     std::string& error) noexcept;
 
