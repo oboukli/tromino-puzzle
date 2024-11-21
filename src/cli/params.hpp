deacad193b308e18f5e39de59092a91e6f01ee8c
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-11-21 22:50:47 +0100

    refactor: Use `std::size_t` for indexes

--- src/cli/params.hpp
+++ src/cli/params.hpp
@@ -7,16 +7,18 @@
 #ifndef CLI_PARAMS_HPP
 #define CLI_PARAMS_HPP
 
+#include <cstddef>
+
 namespace tromino::cli::params {
 
 constexpr int const REQUIRED_ARG_COUNT{4};
 
-constexpr int const ORDER_ARG_IDX{1};
-constexpr int const MARKX_ARG_IDX{2};
-constexpr int const MARKY_ARG_IDX{3};
+constexpr std::size_t const ORDER_ARG_IDX{1U};
+constexpr std::size_t const MARKX_ARG_IDX{2U};
+constexpr std::size_t const MARKY_ARG_IDX{3U};
 
 #ifdef _WIN64
-constexpr int const USE_WCH_ARG_IDX{4};
+constexpr std::size_t const USE_WCH_ARG_IDX{4U};
 #endif // _WIN64
 
 } // namespace tromino::cli::params
