4088125eb9d64781bc9e4de745e063352a4222e6
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-11-21 22:20:29 +0100

    refactor: Remove forward declaration

---
+++ src/gui/shared_state.hpp
@@ -0,0 +1,14 @@
+#ifndef GUI_SHARED_STATE_HPP
+#define GUI_SHARED_STATE_HPP
+
+#include <vector>
+
+#include <tromino/gfx2d/step.hpp>
+
+namespace tromino::tromino2d {
+
+using SharedState = std::vector<tromino::gfx2d::Step>;
+
+} // namespace tromino::tromino2d
+
+#endif // GUI_SHARED_STATE_HPP
