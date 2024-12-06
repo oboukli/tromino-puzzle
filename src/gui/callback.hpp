55b34a5a7474ee67a663011ba36478847ba5895b
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-12-06 22:15:17 +0200

    refactor: Rename symbols

--- src/gui/callback.hpp
+++ src/gui/callback.hpp
@@ -8,17 +8,17 @@
 #define GUI_CALLBACK_HPP_
 
 template <typename T>
-using tromino_cb_t
+using tromino_callback_t
     = auto (*)(int pos_x, int pos_y, int flip_x, int flip_y, T* state) noexcept
     -> void;
 
 template <typename T>
-struct SolverState final {
+struct solver_state_wrapper_t final {
     T* state{};
-    tromino_cb_t<T> callback{};
+    tromino_callback_t<T> callback{};
 };
 
-extern "C" void solve_puzzle_cb(
+extern "C" void tromino_solve_puzzle_cb(
     int pos_x, int pos_y, int flip_x, int flip_y, void* state
 ) noexcept;
 
